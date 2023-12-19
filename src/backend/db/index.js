99669e11d0f1e2da397f3a1b6c6d011828b28325
Yufan <e0773591@u.nus.edu>
2023-12-19 16:42:09 +0800

    Fix DB index.js Logic

--- src/backend/db/index.js
+++ src/backend/db/index.js
@@ -1,30 +1,28 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const mongoose = require('mongoose');
 const path = require('path');
 
 // This function initializes the database connection
 async function connectToDatabase() {
+  // Set the path to the .env file
   require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
-  
+
   const uri = process.env.MONGODB_URI;
-  console.log('URI:', uri); // Debug statement to print the MongoDB URI
-  
   if (!uri) {
     throw new Error('The MONGODB_URI environment variable is not set.');
   }
-  
-  const client = new MongoClient(uri, {
-    tls: true,
-    serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-    }
-  });
 
+  // Use mongoose to connect to the database
   try {
-    await client.connect();
+    // Connect to the MongoDB server with the provided URI
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 5000 // Timeout after 5 seconds instead of 30
+    });
     console.log("Connected successfully to MongoDB!");
-    return client;
+    
+    // Return the mongoose connection
+    return mongoose.connection;
   } catch (error) {
     console.error("Connection to MongoDB failed:", error);
     throw error; // Rethrow the error to handle it in the calling code
